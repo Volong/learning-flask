030f67a03314c69863da35d23aca8611047106b9
volong <yhlona523@gmail.com>
2019-05-03 23:05:18 +0800

    :smile:hello 2b

--- chapter02/hello.py
+++ chapter02/hello.py
@@ -1,3 +1,6 @@
+#coding=utf-8
+
+'动态路由'
 
 from flask import Flask
 
@@ -6,7 +9,12 @@ app = Flask(__name__)
 @app.route('/')
 
 def index():
-	return '<h1>Hello World</h1>';
+	return '<h1>Hello World</h1>'
+
+@app.route('/user/<name>')
+
+def user(name):
+	return '<h1>Hello, %s!</h1>' % name 
 
 if __name__ == '__main__':
-	app.run(debug=True);
+	app.run(debug=True)
